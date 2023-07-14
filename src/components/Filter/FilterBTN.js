bf96a84e308510d2e41dfe1c34cd79e44c8d51ed
PoojithaMiriyala <mpoojitharao@gmail.com>
2023-07-15 01:48:04 +0530

    Incorporating Changes

--- src/components/Filter/FilterBTN.js
+++ src/components/Filter/FilterBTN.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const FilterBTN = ({ input, task, updatePageNumber, index, name }) => {
-  return (
-    <div>
-      <style jsx>
-        {`
+    return (
+        <div>
+            <style jsx>
+                {`
           .x:checked + label {
             background-color: #0b5ed7;
             color: white;
@@ -13,28 +13,28 @@ const FilterBTN = ({ input, task, updatePageNumber, index, name }) => {
             display: none;
           }
         `}
-      </style>
+            </style>
 
-      <div className="form-check">
-        <input
-          className="form-check-input x"
-          type="radio"
-          name={name}
-          id={`${name}-${index}`}
-        />
-        <label
-          onClick={(x) => {
-            task(input);
-            updatePageNumber(1);
-          }}
-          className="btn btn-outline-primary"
-          for={`${name}-${index}`}
-        >
-          {input}
-        </label>
-      </div>
-    </div>
-  );
+            <div className="form-check">
+                <input
+                    className="form-check-input x"
+                    type="radio"
+                    name={name}
+                    id={`${name}-${index}`}
+                />
+                <label
+                    onClick={(x) => {
+                        task(input);
+                        updatePageNumber(1);
+                    }}
+                    className="btn btn-outline-primary"
+                    for={`${name}-${index}`}
+                >
+                    {input}
+                </label>
+            </div>
+        </div>
+    );
 };
 
 export default FilterBTN;
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 
 const Pagination = ({ pageNumber, info, updatePageNumber }) => {
-  let pageChange = (data) => {
-    updatePageNumber(data.selected + 1);
-  };
+    let pageChange = (data) => {
+        updatePageNumber(data.selected + 1);
+    };
 
-  const [width, setWidth] = useState(window.innerWidth);
-  const updateDimensions = () => {
-    setWidth(window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
-  }, []);
+    const [width, setWidth] = useState(window.innerWidth);
+    const updateDimensions = () => {
+        setWidth(window.innerWidth);
+    };
+    useEffect(() => {
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
 
-  return (
-    <>
-      <style jsx>
-        {`
+    return (
+        <>
+            <style jsx>
+                {`
           @media (max-width: 768px) {
             .pagination {
               font-size: 12px;
@@ -34,24 +34,24 @@ const Pagination = ({ pageNumber, info, updatePageNumber }) => {
             }
           }
         `}
-      </style>
-      <ReactPaginate
-        className="pagination justify-content-center my-4 gap-4"
-        nextLabel="Next"
-        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-        previousLabel="Prev"
-        previousClassName="btn btn-primary fs-5 prev"
-        nextClassName="btn btn-primary fs-5 next"
-        activeClassName="active"
-        marginPagesDisplayed={width < 576 ? 1 : 2}
-        pageRangeDisplayed={width < 576 ? 1 : 2}
-        pageCount={info?.pages}
-        onPageChange={pageChange}
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-      />
-    </>
-  );
+            </style>
+            <ReactPaginate
+                className="pagination justify-content-center my-4 gap-4 text-white"
+                nextLabel="Next"
+                forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+                previousLabel="Prev"
+                previousClassName="btn btn-primary fs-5 prev"
+                nextClassName="btn btn-primary fs-5 next"
+                activeClassName="active"
+                marginPagesDisplayed={width < 576 ? 1 : 2}
+                pageRangeDisplayed={width < 576 ? 1 : 2}
+                pageCount={info?.pages}
+                onPageChange={pageChange}
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+            />
+        </>
+    );
 };
 
 export default Pagination;
